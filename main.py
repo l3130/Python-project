036b92007ce4190b5f46665a4505198de4d1a1a3
l3130 <kmett32@gmail.com>
2026-07-12 10:20:41 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+name = "Lanre Adefala"
+age = 59
+favorite_language = "Python"
+
+print("=" * 35)
+print("Welcome to Python")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Favorite Language: {favorite_language}")
+print("=" * 35)
