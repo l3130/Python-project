b1da96a82ace3fe60320184e0bada71169124c48
l3130 <kmett32@gmail.com>
2026-07-17 18:25:13 +0100

    Lesson 2: Learn user input

--- main.py
+++ main.py
@@ -19,3 +19,28 @@ print(f"Favorite Language: {favorite_language}")
 print("=" * 35)
 
 print("I am learning Python one lesson at a time.")
+
+name = input("What is your name? ")
+
+print(f"Hello, {name}!")
+
+favorite_food = input("What is your favorite food? ")
+
+print(f"You like {favorite_food}.")
+
+age = int(input("How old are you? "))
+print(f"You are {age} years old.")
+
+print("=" * 40)
+name = input("What is your name? ")
+print(f"Name: {name}!")
+age = int(input("How old are you? "))
+print(f"Age: {age}")
+favorite_language = input("What is your favorite programming language? ")
+print(f"Favorite Language: {favorite_language}.")
+print("=" * 40)
+
+country = input("What is your country? ")
+city = input("What city do you live in? ")
+print(f"{name} lives in {city}, {country}.")
+
