4aef5186012f096675a8a9c3afc138e802812438
l3130 <kmett32@gmail.com>
2026-07-17 17:07:43 +0100

    Reset project for Python roadmap

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-print("Welcome to Professional Python")
+# ==========================================
+# Python Developer Roadmap
+# Lesson 1: Variables and Output
+# Student: Lanre Adefala
+# ==========================================
+
+
+print("Welcome to the Python Developer Roadmap!")
+
+# Variables
 name = "Lanre Adefala"
 age = 59
 favorite_language = "Python"
 
 print("=" * 35)
-print("Welcome to Python")
 print(f"Name: {name}")
 print(f"Age: {age}")
 print(f"Favorite Language: {favorite_language}")
 print("=" * 35)
-print("I am learning Git!")
+
+print("I am learning Python one lesson at a time.")
