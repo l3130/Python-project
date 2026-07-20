ec5c6ff4ed05dd87f5d2ff49c1dc8c3fdb4bac55
l3130 <kmett32@gmail.com>
2026-07-20 10:49:35 +0100

    Add Lesson 3: Numbers and Arithmetic

---
+++ Lesson3/main.py
@@ -0,0 +1,32 @@
+# ==========================================
+# Python Developer Roadmap
+# Lesson 3: Numbers and Arithmetic
+# Student: Lanre Adefala
+# ==========================================
+
+print("Lesson 3: Numbers and Arithmetic")
+
+a = 20
+b = 6
+
+print("=" * 35)
+print(f"a = {a}")
+print(f"b = {b}")
+print("=" * 35)
+
+print(f"Addition: {a + b}")
+print(f"Subtraction: {a - b}")
+print(f"Multiplication: {a * b}")
+print(f"Division: {a / b}")
+print(f"Floor Division: {a // b}")
+print(f"Remainder: {a % b}")
+print(f"Power: {a ** b}")
+
+
+age = int(input("Enter your age: "))
+
+number = int(input("Enter a number: "))
+
+print(f"You entered {number}")
+print(f"Double = {number * 2}")
+print(f"Square = {number ** 2}")
